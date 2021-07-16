3ff6339b03cfccc05e3ae2d13ee6d6840c20c7ad
codershona <falgunimst95@gmail.com>
2021-07-17 00:46:45 +0200

    installation of vuetify 3 and advanced webpack

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from "vue";
+import Vue from 'vue'
+import vuetify from "./plugins/vuetify";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(vuetify);
+
+app.mount("#app");
+
+// src/main.js
+new Vue({
+  vuetify,
+}).$mount('#app')
+
