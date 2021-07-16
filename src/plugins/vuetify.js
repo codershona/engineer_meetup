f6fbaa80c9d784595b51b52d0d08a57fa2be71f3
codershona <falgunimst95@gmail.com>
2021-07-17 00:52:57 +0200

    trying to setup the project

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/lib/styles/main.sass";
-import { createVuetify } from "vuetify";
+// import { createVuetify } from "vuetify";
 import * as components from "vuetify/lib/components";
 import * as directives from "vuetify/lib/directives";
 // src/plugins/vuetify.js
@@ -17,7 +17,7 @@ const opts = {}
 
 export default new Vuetify(opts)
 
-export default createVuetify({
-  components,
-  directives,
-});
+// export default createVuetify({
+//   components,
+//   directives,
+// });
