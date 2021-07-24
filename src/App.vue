62320ca0b133bc4834f9cf90ffd1bbfe1e517dbc
codershona <falgunimst95@gmail.com>
2021-07-24 21:02:54 +0200

    working in button UI by adding groups icons

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,22 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
       <v-toolbar-title>Engineer's MeetUp</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
+      <v-btn
+      depressed
+      elevation="2"
+      large
+      class="ma-2"
+      color="warning"
+    >
+      View Events
+      <v-icon
+          dark
+          right
+        >
+          mdi-wrench
+        </v-icon>
+    </v-btn>
       <v-btn icon>
         <v-icon>mdi-export</v-icon>
       </v-btn>
