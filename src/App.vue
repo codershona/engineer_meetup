7f907e68951c097a77bc1110c489d2040d966d4f
codershona <falgunimst95@gmail.com>
2021-07-24 21:30:26 +0200

    working in responsive menu by adding vuetify side navigation and its drawers sidebar menu UI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <v-app>
+     <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+    <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-icon
+          dark
+          left
+          color="primary"
+        >
+          mdi-wrench
+        </v-icon>
+            <v-list-item-title>View Events</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+    </v-navigation-drawer>
     <div>
     <v-toolbar
       dark
       prominent
       src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.native.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Engineer's MeetUp</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -26,13 +65,13 @@
         </v-icon>
     </v-btn>
       <v-btn icon>
-        <v-icon>mdi-export</v-icon>
+       <v-icon>mdi-export</v-icon>
       </v-btn>
     </v-toolbar>
   </div>
 
     <v-main>
-
+      <!--  -->
     </v-main>
   </v-app>
 </template>
@@ -41,6 +80,10 @@
 
 
 export default {
-
-};
+  data () {
+    return {
+      drawer: false
+    }
+  }
+}
 </script>
