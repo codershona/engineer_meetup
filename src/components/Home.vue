438b4887eaba7b68a75821cec199d0d75d06c033
codershona <falgunimst95@gmail.com>
2021-07-27 20:01:11 +0200

    working with carousel in the home page: using carousel component With the cycle prop you can have your slides automatically transition to the next available every 6s (default)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
          <v-flex
          xs12
          sm6
-         class="text-sm-right"
+         class="text-xs-center text-sm-right"
          >
                 <v-btn
                 class="ma-2"
@@ -21,7 +21,11 @@
                   Expore Events
                 </v-btn>
             </v-flex>
-            <v-flex xs12 sm6>
+            <v-flex
+            xs12
+            sm6
+            class="text-xs-center text-sm-left"
+            >
                 <v-btn
                 class="ma-2"
                 large
@@ -33,5 +37,81 @@
                 </v-btn>
             </v-flex>
         </v-layout>
+    <v-layout
+    row
+    wrap
+    >
+        <v-flex
+        xs12
+        >
+       <v-carousel
+       height="300"
+       hide-delimiter-background
+       show-arrows-on-hover
+       cycle
+       >
+        <v-carousel-item
+         v-for="meetup in meetups"
+         :key="meetup.id"
+         :src="meetup.imageUrl"
+         elevation="24"
+         max-width="444"
+         class="mx-auto"
+    >
+     <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+    <div
+    class="title"
+    >
+      {{ meetup.title }}
+    </div>
+    </v-row>
+    </v-carousel-item>
+  </v-carousel>
+  </v-flex>
+        </v-layout>
     </v-container>
 </template>
+
+
+<script>
+export default {
+    data () {
+        return {
+            meetups: [
+                {
+                imageUrl: 'https://images.pexels.com/photos/6315188/pexels-photo-6315188.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', id: 'afanedwe121',
+                title: 'Events in New York'
+                },
+                {
+                imageUrl: 'https://images.pexels.com/photos/7169637/pexels-photo-7169637.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', id: 'afanedwe122',
+                title: 'Events in Canada'
+                },
+                {
+                imageUrl: 'https://images.pexels.com/photos/2526105/pexels-photo-2526105.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', id: 'afanedwe123',
+                title: 'Events in Australia'
+                },
+                {
+                imageUrl: 'https://images.pexels.com/photos/7245330/pexels-photo-7245330.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', id: 'afanedwe124',
+                title: 'Events in London'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+  .title {
+      position: absolute;
+      bottom: 50px;
+      background-color: rgba(0,0,0,0.5);
+      color: white;
+      font-size: 2.2em;
+      padding: 20px;
+  }
+</style>
