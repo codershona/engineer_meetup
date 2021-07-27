f809101cbf7ba72f9654b158d89027a186fd0e02
codershona <falgunimst95@gmail.com>
2021-07-27 19:14:06 +0200

    working with carousel in the home page: using grid , layout and button UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        <p>This is first home page</p>
-    </div>
+    <v-container>
+        <v-layout
+        row
+        wrap
+        no-gutters
+        style="height: 150px;"
+        >
+         <v-flex xs12 sm6>
+                <v-btn
+                large
+                dark
+                router to="/meetups"
+                class="pink"
+                >
+                  Expore Events
+                </v-btn>
+            </v-flex>
+            <v-flex xs12 sm6>
+                <v-btn
+                large
+                dark
+                router to="/meetup/new"
+                class="teal"
+                >
+                  Organize Events
+                </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
